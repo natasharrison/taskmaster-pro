2c3206a8581f8f06a2591474833d63f2967f8420
natasharrison <natasharrison@gmail.com>
2021-04-08 15:22:41 -0700

    datepicker and audit tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date 
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -44,6 +46,24 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var auditTask = function(taskEl) {
+  // get date from task element 
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm 
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element 
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)){
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <=2){
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 // enable draggable/sortable feature on list-group elements 
 $(".card .list-group").sortable({
   // enable dragging across lists 
@@ -110,6 +130,11 @@ $("#trash").droppable({
   out: function(event, ui) {
     console.log("ui");
   }
+});
+
+// convert text field into a jquery date picker
+$("#modalDueDate").datepicker({
+  minDate:1
 });
 
 // modal was triggered
@@ -204,12 +229,21 @@ $(".list-group").on("click", "span", function() {
   // swap out elements 
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1, 
+    onClose: function(){
+      // when calendar is closed, force a 'change' event on the 'dateInput"
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new element 
   dateInput.trigger("focus");
 });
 
 // value of due date was changed 
-$(".list-group").on("blur", "input[type='text]", function(){
+$(".list-group").on("change", "input[type='text]", function(){
   // get current text 
   var date = $(this)
     .val()
@@ -234,9 +268,9 @@ $(".list-group").on("blur", "input[type='text]", function(){
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(date);
-
-  // replace input with span element 
-  $(this).replaceWith(taskSpan);
+    // replace input with span element 
+    $(this).replaceWith(taskSpan);
+    auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
@@ -247,6 +281,7 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
 
 // load tasks for the first time
 loadTasks();
